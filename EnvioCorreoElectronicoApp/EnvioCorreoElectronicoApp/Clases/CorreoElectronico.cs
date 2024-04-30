c665b918b5cd47a943dbd63f0d2159d2f84f7d13
julioagu@hotmail.com <julioagu@hotmail.com>
2024-04-30 19:40:46 -0400

    Adicion Email

---
+++ EnvioCorreoElectronicoApp/EnvioCorreoElectronicoApp/Clases/CorreoElectronico.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnvioCorreoElectronicoApp.Clases
+{
+    public class CorreoElectronico
+    {
+
+        public void EnviarCorreoElectronico(string destinatario, string mensaje)
+        {
+            // Lógica para enviar el correo electrónico
+        }
+
+    }
+}
---
+++ EnvioCorreoElectronicoApp/EnvioCorreoElectronicoApp/Clases/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EnvioCorreoElectronicoApp.Clases
+{
+    public class Persona
+    {
+
+        private string nombre;
+        private int edad;
+        private string direccion;
+        private string correoElectronico;
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"Direccion: {direccion}");
+            Console.WriteLine($"Correo Electronico: {correoElectronico}");
+        }
+
+    }
+}
